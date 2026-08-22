e0460b1038569380991da6965fe330f108b8273e
Kashish <kgupta7827@gmail.com>
2026-08-22 17:24:02 +0530

    Update .gitignore and add arXiv extraction script

---
+++ lib/tools/arxiv.ts
@@ -0,0 +1,10 @@
+//Script that takes a local PDF or arXiv link, extracts text, and isolates 1 key equation or baseline metric table.
+export class arXiv {
+    private paperLink:string 
+    constructor(paperlink:string){
+        this.paperLink = paperlink
+    }
+    extractParams(){
+
+    }
+}
